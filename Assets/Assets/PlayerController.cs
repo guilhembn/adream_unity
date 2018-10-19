32bc084e5eb50ddbc0f154311393a6d37f20cb6b
guilhembn <gbuisan@laas.fr>
2018-10-19 17:37:14 +0200

    First commit

---
+++ Assets/Assets/PlayerController.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+//[RequireComponent(typeof(Animator))]
+//[RequireComponent(typeof(ConfigurableJoint))]
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 5f;
+	[SerializeField]
+	private float lookSensitivity = 3f;
+
+	[SerializeField]
+	private LayerMask environmentMask;
+
+	// Component caching
+	private PlayerMotor motor;
+
+	void Start ()
+	{
+		motor = GetComponent<PlayerMotor>();
+	}
+
+	void Update ()
+	{
+
+
+		if (Cursor.lockState != CursorLockMode.Locked)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		//Calculate movement velocity as a 3D vector
+		float _xMov = Input.GetAxis("Horizontal");
+		float _zMov = Input.GetAxis("Vertical");
+
+		Vector3 _movHorizontal = transform.right * _xMov;
+		Vector3 _movVertical = transform.forward * _zMov;
+
+		// Final movement vector
+		Vector3 _velocity = (_movHorizontal + _movVertical) * speed;
+
+		
+
+		//Apply movement
+		motor.Move(_velocity);
+
+		//Calculate rotation as a 3D vector (turning around)
+		float _yRot = Input.GetAxisRaw("Mouse X");
+
+		Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+		//Apply rotation
+		motor.Rotate(_rotation);
+
+		//Calculate camera rotation as a 3D vector (turning around)
+		float _xRot = Input.GetAxisRaw("Mouse Y");
+
+		float _cameraRotationX = _xRot * lookSensitivity;
+
+		//Apply camera rotation
+		motor.RotateCamera(_cameraRotationX);
+
+
+	}
+
+
+}
